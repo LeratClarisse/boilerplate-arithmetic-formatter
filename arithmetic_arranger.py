6696bdb1c8c76654d9ca015db3f67a8e2a0897ff
Lerat Clarisse <clarisselerat@hotmail.fr>
2022-06-07 12:53:33 +0000

    Result line + debug OK

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,15 +1,18 @@
-def arithmetic_arranger(problems):
+def arithmetic_arranger(problems, with_result = False):
   #init
   first_line = ""
   second_line = ""
   dash_line = ""
   result_line = "\n"
+  is_last = False
+  i = 0
 
   if len(problems) > 5:
     return "Error: Too many problems."
   
   for string in problems:
     #extract datas
+    is_last = False if i < (len(problems) - 1) else True
     datas = string.split(" ")
     n1 = datas[0]
     n2 = datas[2]
@@ -23,21 +26,31 @@ def arithmetic_arranger(problems):
       
     #update each line
     first_line += construct_first_line(n1, len(n2))
-    first_line += "    "
 
     second_line += construct_second_line(len(n1), n2, sign)
-    second_line += "    "
   
-    longest_number = n2 if n1 < n2 else n1
-    dash_line += construct_dash_line(len(longest_number))
+    dash_line += construct_dash_line(len(n1), len(n2))
+
+    if not is_last:
+      first_line += "    "
+      second_line += "    "
+      dash_line += "    "
+      
   
-    if len(problems) > 1:
-      result_line += construct_result_line(n1, n2, sign)
+    if with_result is True:
+      result_line += construct_result_line(int(n1), int(n2), sign)
+      if not is_last:
+        result_line += "    "
+
+    
+    i += 1
   
   #return formatted result
-  #arranged_problems = first_line + "\n" + second_line + "\n" + dash_line + result_line
-  arranged_problems = first_line + "\n" + second_line
-  
+  arranged_problems = first_line + "\n" + second_line + "\n" + dash_line
+
+  if with_result is True:
+    arranged_problems += result_line
+ 
   return arranged_problems
 
 
@@ -82,8 +95,34 @@ def construct_second_line(first_number_len, second_number, sign):
   result += second_number
   return result
 
-def construct_dash_line(longest_number_length):  
-  return ""
+def construct_dash_line(first_number_len, second_number_len):
+  result = ""
+  
+  if first_number_len <= second_number_len:
+    result += '-'*(second_number_len + 2)
+  else:
+    result += '-'*(first_number_len + 2)
+      
+  return result
 
-def construct_result_line(first_number, second_number, sign):  
-  return ""
+def construct_result_line(first_number, second_number, sign):
+  operation_result = 0
+  result = ""
+  longest_number = first_number
+  
+  if second_number > first_number:
+    longest_number = second_number
+  
+  if sign == '+':
+    operation_result = first_number + second_number
+  else:
+    operation_result = first_number - second_number
+
+  i = 0
+  while i < ((2 + len(str(longest_number))) - len(str(operation_result))):
+    result += " "
+    i += 1
+
+  result += str(operation_result)
+    
+  return result
